cafc5067c221ae6246ba9dcbb56aaa37576b22c1
Djorr <djorraw@gmail.com>
2023-05-25 09:49:58 +0200

    Latest version
    - Removed scripts

---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game { 'rdr3', 'gta5' }
+
+author 'Djorr <contact@rubixstudios.nl>'
+description 'A radio that allows you to claim a frequency and see who joined your frequency.'
+version '1.0.0'
+
+server_script 'server.lua'
